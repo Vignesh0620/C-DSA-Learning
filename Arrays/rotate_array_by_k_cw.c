801c58aad2502e7688202f2d04915c1463d4fea3
Vignesh0620 <vickydakshin2001@gmail.com>
2025-12-05 22:09:53 +0530

    Array01 - Commit
    Solved 5 problems
    - finding maximum and minimum in an array  - max_min.c
    - finding numbers in array are positive/negative and odd/even -
      even_odd_pos_neg_vals.c
    - reversing the elements in an array - reverse_array.c
    - rotate array clockwise by k elements - rotate_array_by_k_cw.c
    - rotate array counter clockwise by k elements - rotate_array_by_k_ccw.c
    
    Signed-off-by: Vignesh0620 <vickydakshin2001@gmail.com>

---
+++ Arrays/rotate_array_by_k_cw.c
@@ -0,0 +1,63 @@
+//Rotate the array by k times to the right (clockwise rotation)
+/*
+    Sample testcase
+    Input: 1 2 3 4 5 6 7, k = 3
+Output: 5 6 7 1 2 3 4
+
+    Logic :first reverse full array , then index from 0 to k-1 and then reverse index k to n-1
+    */
+#include <stdio.h>
+
+int rotate_by_k(int arr[], int n, int k);
+
+int main()
+{
+    int num,k;
+    printf("Enter number of elements in an array ");
+    scanf("%d",&num);
+    int arr[num];
+    for(int i=0;i<num;i++)
+    {
+        printf("Enter Element %d:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter value to rotate array by k");
+    scanf("%d",&k);
+
+    rotate_by_k(arr,num,k);
+
+    return 0;
+}
+
+int rotate_by_k(int arr[], int n,int k)
+{
+    //Reverse whole array first
+    for(int i=0,j=n-1;i<n/2;i++,j--)
+    {
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    //reverse 0 to k-1 elements in the array
+    for(int i=0,j=k-1;i<j;i++,j--)
+    {
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    //reverse kth to n elements in the array
+    for(int i=k,j=n-1;i<j;i++,j--)
+    {
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    printf("The array after moving right by k rotation : \n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
